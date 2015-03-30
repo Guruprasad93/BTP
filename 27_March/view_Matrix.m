fd5f2424c7f211f71b06893f0e857fd63488a70d
Guruprasad93 <gpr.1993@gmail.com>
2015-03-30 11:14:21 +0530

    viewing matrix + vars

---
+++ 27_March/view_Matrix.m
@@ -0,0 +1,5 @@
+for j =1:l_matrix
+    for k =1:b_matrix
+	disp(['SIM:' num2str(Sim_Matrix(k,j)) ' Mig_index:' num2str(mig_index(k,j)) ' Pers_ctr:' num2str(persctr(k,j)) ' Div_ctr:' num2str(divctr(k,j)) ' Position:' num2str(k) ' ' num2str(j) ' Pores:' num2str(porous_matrix(k,j))])
+    end
+end
