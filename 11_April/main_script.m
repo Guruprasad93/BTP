59074a601f6afc8bafa528a1cd75fee2f18139f4
Guruprasad93 <gpr.1993@gmail.com>
2015-04-13 16:53:47 +0530

    Main script to be run

---
+++ 11_April/main_script.m
@@ -0,0 +1,11 @@
+%Run all scripts here!
+run('global_variables');
+run('initialize_values');
+run('Matrix_porosity');
+run('Seed_Matrix_uniform')
+%run('view_Matrix');
+disp('\n\n');
+%gridsvisited
+run('start_automaton');
+%clear
+
