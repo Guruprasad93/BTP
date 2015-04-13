5998c87f0ace78ce7252225cb72301e20a451332
Guruprasad93 <gpr.1993@gmail.com>
2015-04-13 16:50:12 +0530

    Setting all global variables

---
+++ 11_April/global_variables.m
@@ -0,0 +1,17 @@
+global l_matrix
+global b_matrix
+global T_Div
+global Sim_Matrix
+global mig_index
+global persctr
+global divctr
+global glc_ctr
+global starve_time
+global gridsvisited
+gridsvisited = char(gridsvisited);
+global porous_matrix
+porous_matrix = char(porous_matrix);
+global collide_persctr
+global collide_persctr_porous
+global stationary_persctr
+global checkpersctr
